9f1ba2fb611aba0a7f2da5e3ae850659e5c8b6c1
zlatn <zlatn91@naver.com>
2025-09-09 17:02:00 +0900

    refactor: simplify RootLayout and add MyView component for safe area handling

--- app/calendar/page.tsx
+++ app/calendar/page.tsx
@@ -10,6 +10,7 @@ export default function CalendarPage() {
     return (
         <MyView>
             <SectionList
+                // showsVerticalScrollIndicator={false}
                 initialScrollIndex={3}
                 initialNumToRender={2}
                 getItemLayout={(data, index) => (
@@ -17,17 +18,21 @@ export default function CalendarPage() {
                 )}
                 keyExtractor={(item, index) => index.toString()}
                 scrollEnabled={true}
-                // showsVerticalScrollIndicator={false}
                 scrollIndicatorInsets={{right: 1}}
                 contentContainerStyle={{gap: 4}}
                 sections={TEST_DATA}
                 stickySectionHeadersEnabled={true}
-
-                renderSectionHeader={(info) => {
-                    return <View style={{backgroundColor: 'black'}}>
+                renderSectionHeader={(info) =>
+                    <View style={{backgroundColor: 'black'}}>
                         <Text style={{color: 'white', textAlign: 'center'}}>HEADER</Text>
                     </View>
-                }}
+                }
+                stickyHeaderHiddenOnScroll={true}
+                renderSectionFooter={(info) =>
+                    <View style={{backgroundColor: 'black'}}>
+                        <Text style={{color: 'white', textAlign: 'center'}}>FOOTER</Text>
+                    </View>
+                }
                 renderItem={({item, section}) => {
                     return (
                         <View style={{height: 300, borderWidth: 2, padding: 8, marginLeft: 4, marginRight: 4}}>
@@ -38,7 +43,6 @@ export default function CalendarPage() {
                     )
                 }}>
             </SectionList>
-            <Text>Calendar</Text>
         </MyView>
     )
 }
