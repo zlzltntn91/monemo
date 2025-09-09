97bae696091cf078147441bab734581afb066449
zlatn <zlatn91@naver.com>
2025-09-09 16:14:35 +0900

    refactor: simplify RootLayout and add MyView component for safe area handling

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import MyView from "@/component/myView";
+
+export default function Index() {
+  return (
+    <MyView>
+        <Text>Index</Text>
+    </MyView>
+  );
+}
